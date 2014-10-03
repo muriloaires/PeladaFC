19f583ab7080068c2f06b5ab580a214356ccd241
Alexandre Mota <alexandremota.es@outlook.com>
2014-10-03 04:12:36 -0300

    Pegando texto do form

--- PeladaFC/js/script.js
+++ PeladaFC/js/script.js
@@ -7,3 +7,11 @@ $('#jogo-rapido').click(function() {
 	$.mobile.changePage("#pagina-jogo-rapido", "fade");
 });
 });
+
+$(function(){
+
+$( "#formularioInserir" ).submit(function( event ) {
+var a = $( "#un" ).val();
+  alert(a);
+});
+});
