42f4f80816aa971160c6ff8816ee248c7f5bb1c9
Alexandre Mota <alexandremota.es@outlook.com>
2014-10-12 07:45:42 -0300

    Criando esqueleto das novas telas, adicionando imagens e finalizando tela de chegada de jogadores com data e hora

--- PeladaFC/js/script.js
+++ PeladaFC/js/script.js
@@ -11,10 +11,18 @@ $('#jogo-rapido').click(function() {
 $(function(){
 
 $( "#btInserir" ).click(function( event ) {
-var a = $( "#un" ).val();
-  alert(a);
+var player = $( "#un" ).val();
+$( "#un" ).val("");
+
 $( "#popupInserir" ).popup( "close" );
-var listItem = '<li><a href="#"><img src="img/jogador.png" class="ui-li-icon">'+a+'</a></li>';
+var currentdate = new Date(); 
+var datetime = "" + currentdate.getDate() + "/"
+            + (currentdate.getMonth()+1)  + "/" 
+            + currentdate.getFullYear() + " - "  
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+var listItem = '<li><img src="img/jogador.png" class="ui-li-icon">'+player+'<span class="ui-li-count">'+datetime+'</span></li>';
  $('ul').append(listItem);
  $("#lista-chegada").listview("refresh");
 });
